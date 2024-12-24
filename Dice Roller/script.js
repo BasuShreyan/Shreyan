cc6de2512628a84e59891c15edd5b3abf2d50876
shreyan <shreyanbasu@outlook.in>
2024-12-24 15:17:49 +0530

    Dice Roller Completed

--- Dice Roller/script.js
+++ Dice Roller/script.js
@@ -1,10 +1,18 @@
 const dice=document.getElementById("num");
 const picture=document.getElementById("picture");
+let para=document.getElementById('para');
+let heading=document.getElementById("heading");
 let numbers=[];
 let images=[];
 let diceNumber;
 
 function numberDice(){
     diceNumber=Number(dice.value);
-    console.log(diceNumber);
+    for(let i=0; i<=diceNumber; i++){
+        numbers.push(Math.floor(Math.random()*6)+1);
+        images.push(`<img src="${numbers[i]}.png" alt=${numbers[i]}>`);
+    }
+    picture.innerHTML=images.join(' ');
+    heading.textContent=`You have rolled:`;
+    para.textContent=numbers.join(',');
 }
